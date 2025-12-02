c01e5b33bc3fc8ce89f9b004ca4c924631937e7d
Gina White <ginabythebay@gmail.com>
2025-12-02 16:19:39 +0100

    worksish

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import dataclasses
 import sys
 
 import click
@@ -7,10 +8,16 @@ import time
 from pathlib import Path
 from datetime import date, datetime, timedelta, timezone
 
-from typing import Iterable, Tuple, Optional, Callable
+from typing import Iterable, Tuple, Optional, Callable, List
 
 PDF_DIR = '~/Downloads'
 DST_DIR = '~/gdrive/finances/filed'
+
+@dataclasses.dataclass(frozen=True)
+class SrcPdf:
+    pdf: Path
+    sender: str
+    year: int
 
 def candidate_pdf(c: Path, earliest: float) -> bool:
     if c.suffix != '.pdf':
@@ -18,34 +25,39 @@ def candidate_pdf(c: Path, earliest: float) -> bool:
     return c.stat().st_mtime >= earliest
 
 
-def src_pdfs(dir: str, days: int) -> Iterable[Path]:
+def src_pdfs(dir: str, days: int) -> Iterable[SrcPdf]:
     earliest = oldest_timestamp(days, time.time())
     d = Path(dir).expanduser()
-    return [
-        c for c in d.iterdir()
-        if candidate_pdf(c, earliest)
-    ]
+    result: List[SrcPdf] = []
+    for c in d.iterdir():
+        if not candidate_pdf(c, earliest):
+            continue
+        p = is_src(c)
+        if p:
+            result.append(p)
+    return result
 
 
-def is_src_name(name: str) -> Tuple[Optional[int], Optional[str]]:
+def is_src(pdf: Path) -> Optional[SrcPdf]:
+    name = pdf.stem
     d, _, rest = name.partition('_')
     if not rest:
-        return None, None
+        return None
     if len(d) != 8:
-        return None, None
+        return None
     if not d.isdigit():
-        return None, None
+        return None
     year = int(d[:4])
     if year < 1900:
-        return None, None
+        return None
     month = int(d[4:6])
     if month < 1 or month > 12:
-        return None, None
+        return None
     day = int(d[6:8])
     if day < 1 or day > 31:
-        return None, None
+        return None
     sender, _, rest = rest.partition('_')
-    return year, sender
+    return SrcPdf(pdf, sender, year)
 
 
 def oldest_timestamp(days: int, now: float) -> float:
@@ -61,28 +73,59 @@ def die(msg: str) -> None:
     sys.exit(1)
 
 
+def mkdir_dryrun(new_dir: Path) -> None:
+    click.echo(f'would create {new_dir}')
+
+
+def mv_dryrun(src_pdf: Path, dest_dir: Path) -> None:
+    click.echo(f'would move {src_pdf} -> {dest_dir / src_pdf.name}')
+
+
+def mkdir_real(new_dir: Path) -> None:
+    new_dir.mkdir()
+
+
+def mv_real(src_pdf: Path, dest_dir: Path) -> None:
+    src_pdf.rename(dest_dir / src_pdf.name)
+
+
+@dataclasses.dataclass(frozen=True)
+class Filer:
+    mkdir_fn: Callable[[Path], None]
+    mv_fn: Callable[[Path, Path], None]
+
+
 @click.command()
 @click.option('--days', default=1, type=click.IntRange(1))
 @click.option('--skip_invalid_names', '-s', is_flag=True)
 @click.option('--force', '-f', is_flag=True)
-def main(days: int, skip_invalid_names: bool, force: bool) -> None:
+@click.option('--dryrun', '-d', is_flag=True)
+def main(days: int, skip_invalid_names: bool, force: bool, dryrun) -> None:
+    filer = Filer(mkdir_dryrun, mv_dryrun) if dryrun else Filer(mkdir_real, mv_real)
     file_root = Path(DST_DIR).expanduser()
     if not file_root.is_dir():
         die(f'{file_root} does not exist')
     click.echo(click.style('Hello from fi!', fg='blue'))
     pdfs = list(src_pdfs(PDF_DIR, days))
-    check_all_pdf_names(pdfs, skip_invalid_names, die)
-    missing_dirs = check_all_dests(file_root, pdfs, force, die)
-    for d in missing_dirs:
-        d.mkdir()
-    check_all_dests(file_root, pdfs, force, die)
+    paths = [
+        p.pdf for p in pdfs
+    ]
+    check_all_dests(file_root, pdfs, force, die, filer.mkdir_fn)
+    for pdf in pdfs:
+        dest = file_root / pdf.sender / str(pdf.year)
+        if not dest.is_dir():
+            filer.mkdir_fn(dest)
+        filer.mv_fn(pdf.pdf, dest)
 
 
-def check_all_dests(file_root: Path, pdfs: Iterable[Path], force: bool, die_fn: Callable[[str], None]) -> None:
+def check_all_dests(file_root: Path,
+                    pdfs: Iterable[SrcPdf],
+                    force: bool,
+                    die_fn: Callable[[str], None],
+                    mkdir_fn: Callable[[Path], None]) -> None:
     all_missing = set()
     for p in pdfs:
-        _, sender = is_src_name(p.stem)
-        dest = file_root / sender
+        dest = file_root / p.sender
         if not dest.is_dir():
             all_missing.add(dest)
     if not all_missing:
@@ -91,27 +134,12 @@ def check_all_dests(file_root: Path, pdfs: Iterable[Path], force: bool, die_fn: 
     all_missing = sorted(all_missing)
     if force:
         for m in all_missing:
-            click.echo(f'creating {m}')
-            m.mkdir()
+            mkdir_fn(m)
     else:
         for m in all_missing:
             click.echo('missing destination directory ' + click.style(m, fg='red'), err=True)
         click.echo('\n', err=True)
         die_fn('to automatically create missing directories use the --force option')
-
-
-def check_all_pdf_names(pdfs: Iterable[Path], skip_invalid_names: bool, die_fn: Callable[[str], None]) -> None:
-    invalid_name_cnt = 0
-    for p in pdfs:
-        year, sender = is_src_name(p.stem)
-        if year:
-            print(f'{p} -> {year}/{sender}')
-        if not year:
-            click.echo(click.style(f'unexpected file [{p}]', fg='red'), err=True)
-            invalid_name_cnt += 1
-    if invalid_name_cnt and not skip_invalid_names:
-        files = 'file' if invalid_name_cnt == 1 else 'files'
-        die_fn(f'found {invalid_name_cnt} unexpected {files}.  To continue, use the --skip_invalid_names option')
 
 
 if __name__ == "__main__":
--- tests/test_script.py
+++ tests/test_script.py
@@ -1,17 +1,18 @@
+from pathlib import Path
+
 import pytest
 
 import main
 
 
 def verify_src_name(name: str, expected_year: int, expected_who: str) -> None:
-    year, who = main.is_src_name(name)
-    assert year == expected_year
-    assert who == expected_who
+    path = Path(name)
+    p = main.is_src(path)
+    assert p.year == expected_year
+    assert p.sender == expected_who
 
 def fail_src_name(name: str) -> None:
-    year, who = main.is_src_name(name)
-    assert not year
-    assert not who
+    assert not main.is_src(Path(name))
 
 def test_is_src_name():
     verify_src_name('20251201_bigco', 2025, 'bigco')
